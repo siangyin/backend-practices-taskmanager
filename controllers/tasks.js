a2f04241a205fde17633b8e2c22fc590677d4209
Lee Siang Yin <siangyin916@gmail.com>
2021-12-09 17:59:19 +0800

    💡update/delete: .findOneAndUpdate .delete , for update need {options}

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -20,12 +20,9 @@ const createTask = async (req, res) => {
 };
 
 const getTask = async (req, res) => {
-	//const {id}=req.params
-
 	try {
-		const id = req.params.id;
+		const { id } = req.params;
 		const task = await Task.findOne({ _id: id });
-		console.log(task);
 		if (!task) {
 			return res.status(404).json({ msg: `no task id:${id}` });
 		}
@@ -36,12 +33,36 @@ const getTask = async (req, res) => {
 	}
 };
 
-const updateTask = (req, res) => {
-	res.status(200).send(`update for task id: ${req.params.id}`);
+const updateTask = async (req, res) => {
+	// {option} after req.body allow return latest data, and run validation for new data
+	try {
+		const { id } = req.params;
+		const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		if (!task) {
+			return res.status(404).json({ msg: `no task id:${id}` });
+		}
+
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const deleteTask = (req, res) => {
-	res.status(200).send(`deleting for task id: ${req.params.id}`);
+const deleteTask = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const task = await Task.findOneAndDelete({ _id: id });
+		if (!task) {
+			return res.status(404).json({ msg: `no task id:${id}` });
+		}
+
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 module.exports = { getTasks, createTask, getTask, updateTask, deleteTask };
